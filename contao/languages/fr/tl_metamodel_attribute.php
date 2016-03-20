99a2794b711fb41de45ff2af19a8fc5134c026de
Mini Model <minimodel@metamodel.me>
2016-03-21 00:20:08 +0100

    Updated translations from transifex.

--- contao/languages/fr/tl_metamodel_attribute.php
+++ contao/languages/fr/tl_metamodel_attribute.php
@@ -8,9 +8,8 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-05-08T08:17:19+02:00
+ * last-updated: 2015-07-11T08:03:50+02:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['0']  = 'Champs';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']  = 'Sélectionner un ou plusieurs attributs à combiner';
@@ -20,5 +19,11 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']             = '
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['0']   = 'Forcer la regénération';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']   = 'Cocher si vous voulez que les valeurs combinées soient régénérées à chaque fois que l\'un des champs dépendant est modifié.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['attributes'] = 'Attributs';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['id']         = 'ID';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['meta']       = 'Champs meta';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['pid']        = 'PID';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['sorting']    = 'Tri';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['tstamp']     = 'Timestamp';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['varbase']    = 'Varbase';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['vargroup']   = 'Vargroup';
 
