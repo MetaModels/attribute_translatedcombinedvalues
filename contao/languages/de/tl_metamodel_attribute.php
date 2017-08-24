b719c616497739267a34c6c39c2246f7432c3aed
Mini Model <minimodel@metamodel.me>
2017-08-24 14:44:32 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_attribute.php
+++ contao/languages/de/tl_metamodel_attribute.php
@@ -8,14 +8,13 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-06-19T04:17:31+02:00
+ * last-updated: 2017-08-24T14:44:28+02:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['0']              = 'Felder';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']              = 'Bitte wählen Sie eines oder mehrere Attribute zum kombinieren aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['0']              = 'Formate';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']              = 'Benutzen Sie "%s" oder einen anderen Platzhalter um formatierten text auszugeben. Weitere Informationen finden Sie unter  <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a>. ';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']              = 'Benutzen Sie "%s" oder einen anderen Platzhalter um formatierten Text auszugeben. Weitere Informationen finden Sie unter  <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a>. ';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']                         = 'Attribute';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['0']               = 'Aktualisierung erzwingen';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']               = 'Auswählen, um eine Neuerstellung der kombinierten Werte erzwingen, wenn sich eines der abhängigen Felder ändert.';
