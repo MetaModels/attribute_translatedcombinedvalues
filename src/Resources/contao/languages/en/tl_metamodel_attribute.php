15801421d48f30d86e041c240ce846a463d09313
Ingolf Steinhardt <git@e-spin.de>
2022-05-19 08:39:48 +0200

    Fix attributes MCW

--- src/Resources/contao/languages/en/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/en/tl_metamodel_attribute.php
@@ -1,25 +1,28 @@
 <?php
 
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_translatedcombinedvalues.
  *
- * PHP version 5
- * @package     MetaModels
- * @subpackage  AttributeCombinedValues
- * @author      Andreas Isaak <andy.jared@googlemail.com>
- * @author      David Greminger <david.greminger@1up.io>
- * @copyright   The MetaModels team.
- * @license     LGPL.
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels/attribute_translatedcombinedvalues
+ * @author     Andreas Isaak <andy.jared@googlemail.com>
+ * @author     David Greminger <david.greminger@1up.io>
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedcombinedvalues/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedcombinedvalues'] = 'Translated Combined values.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedcombinedvalues'] = 'Translated combined values.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']                   = array('Fields', 'Please select one or more attributes to combine.');
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']                   = array('Formate', 'Use "%s" or any other placeholder to create a formated text. See <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a> for more informations.');
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']                    = array('Force regenerating', 'Check this, if you want the combined values to be regenerated whenever any of the dependant fields is changed.');
